fe442b3ba20ce0c83339db2494f84e3887ce6fe4
Miguel Dell'Uomini <delldor@gmail.com>
2016-11-28 12:59:36 -0400

    Corregidos errores varios

--- songpress.sh
+++ songpress.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Instalador con packagekit de manejador de letras con acordes en formato chrodpro
 #http://www.skeed.it/songpress
+#HACER: como asociar el tipo de archivos .crd .chordpro
 
 #if ! dpkg-query -l python-wxtools > /dev/null; then
 if ! LANGUAGE=C pkcon search name python-wxtools| grep Installed > /dev/null; then
@@ -11,16 +12,18 @@ fi
 archivo=/var/tmp
 
 wget -P $archivo -Nc https://github.com/lallulli/songpress/archive/master.zip
-unzip $archivo/master.zip
+
+unzip $archivo/master.zip -d /tmp/spress
+
 if [ ! -e /opt/songpress/ ]; then
-sudo sh -c "mkdir -p /opt/songpress
+sudo sh -c "mkdir -p /opt/songpress"
 fi
 
-cp -vur /var/tmp/songpress-master/src/* /opt/songpress
+sudo sh -c "cp -vur /tmp/spress/songpress-master/src/* /opt/songpress
 chmod a+x /opt/songpress/main.py"
 
 #Crea acceso en escritorio y menú
-cat <<EOD >$(xdg-user-dir DESKTOP)/songpress.desktop 
+cat <<EOD >/usr/share/applications/songpress.desktop
 #!/usr/bin/env xdg-open
 [Desktop Entry]
 Version=1.0
@@ -35,6 +38,6 @@ Icon=/opt/songpress/img/songpress.png
 Categories=AudioVideo;
 EOD
 
-sudo cp -v $(xdg-user-dir DESKTOP)/songpress.desktop /usr/share/applications/songpress.desktop
+sudo cp -v /usr/share/applications/songpress.desktop $(xdg-user-dir DESKTOP)/songpress.desktop 
 sudo chmod a+x {/usr/share/applications,$(xdg-user-dir DESKTOP)}/songpress.desktop
 sudo update-menus; sudo xdg-desktop-menu forceupdate
